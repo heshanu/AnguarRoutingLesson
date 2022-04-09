c3f5b4cd795edb2d81a33813155e57addf721395
heshanu <heshanu97@gmail.com>
2022-04-09 12:29:55 -0700

    Added node m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {Routes} from '@angular/router';
 
+const appRoutes:Routes = [
+  {
+    path: '',component:HomeComponent
+  },
+  {
+    path:'users',component:UserComponent
+  }
+];
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
